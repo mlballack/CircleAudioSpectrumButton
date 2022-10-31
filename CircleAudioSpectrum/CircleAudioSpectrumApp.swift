f9f614a9e32cadb8d29a029231a5bc107b4e5cef
Masaki Hayashi <chart13gut@outlook.jp>
2022-10-31 20:45:45 +0900

    Initial Commit

---
+++ CircleAudioSpectrum/CircleAudioSpectrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleAudioSpectrumApp.swift
+//  CircleAudioSpectrum
+//
+//  Created by Masaki on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct CircleAudioSpectrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircleAudioSpectrum/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CircleAudioSpectrum
+//
+//  Created by Masaki on 2022/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
